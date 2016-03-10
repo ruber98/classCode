d7958314a5ed33fd687a20be06af5078e5384e1b
ruber98 <thtghth@gmail.com>
2016-03-10 22:05:49 +0000

    Thursday Working!

--- forLoops/project/script.js
+++ forLoops/project/script.js
@@ -1,2 +1,19 @@
 $( document ).ready(function() {
+
+
+var number = 0;
+
+//  while (number < 100){
+//      $('body').append('<img src="http://exmoorpet.com/wp-content/uploads/2012/08/cat.png"/>');
+//     number ++;
+//     }
+    
+    for (var i = 0; i < 100; i++){
+          $('body').append('<img src="http://exmoorpet.com/wp-content/uploads/2012/08/cat.png"/>');
+        $('body').append('<img src="http://media.mydogspace.com.s3.amazonaws.com/wp-content/uploads/2013/08/puppy-500x350.jpg"/>');
+
+
+
+    }
+    
 });
--- whileLoops/guided/app.js
+++ whileLoops/guided/app.js
@@ -1,8 +1,29 @@
 
 // How do I write a while loop that prints "Justin Bieber" 10 times?
+var count = 0
 
+while (count < 10){
+    console.log("Justin Bieber!");
+
+    count ++;
+};    
 
 // How do I write a while loop that iterates 5 times and prints out the sequence 0 to 4?
+var biscuit = 0
+var hammer = 5
 
+while (biscuit < 5){
+    console.log();
+    biscuit++;
+}
 
 // How do I write a while loop that prints "Can't get enough of the Biebs" forever?
+
+var count = 0;
+
+while(count){
+ ///this will run 
+console.log(count);
+ 
+count += 1;
+}
