bf34b0494c23f12abffda0649e4b01bba6911ec3
Adam Driggers <awdriggs@gmail.com>
2016-03-29 15:56:38 -0400

    tasks

---
+++ arrays1/tasks/tasks.js
@@ -0,0 +1,31 @@
+/* These tasks will allow you to work with arrays.
+ * Specifically we will be iterating through arrays and accessing their 
+ * elements. */
+​
+/* Task1: Using for loops, iterate through array1 to find and print the 
+ * smallest number. */
+​
+var array1 = [5, 8, 92, 0, -5, 13, 9, 450, 2, 3];
+​
+// Your code goes here!
+​
+/* Task2: Write and algorithm to sort array1 in ascending order (smallest to 
+ * largest number). Print the sorted array.*/
+​
+// Your code goes here!
+​
+/* Task3: Add the corresponding indices from array1 to array2 and put those
+ * numbers into an empty  array3. This will be more challenging now that we 
+ * are using three arrays, but not really. Print array3. */
+​
+var array2 = [10, 12, 9, 9, 6, 13, 87, 2, -9, 1];
+var array3 = []
+​
+// Your code goes here!
+​
+/* Task4: The real challenge! Count the number of numbers that appear in both 
+ * array1 and array2. Hints: create a variable that keeps track of the count,
+ * look up the keyword 'continue' in js, and use two for loops. Print the 
+ * number you count. */
+​
+// Your code goes here!
